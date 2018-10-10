3dde14bf0d0cf50367227a4517ffc848e63ede61
Jônatas Deorce <jonatasprog@gmail.com>
2018-10-10 16:05:09 -0300

    Esqueleto inicial do projeto BDD de automação na plataforma  .Net

---
+++ BDDprovaautomacao/feature/ComprarProduto.feature
@@ -0,0 +1,32 @@
+﻿Feature: ComprarProduto
+	In order to buy a product
+	As a user
+	I want to choose an item, sign up me and confirm my purchase
+
+  Scenario: Selecionar um produto na página
+    Given Usuário está na Home Page
+    When Usuário escolhe um produto
+    Then É redirecionado para a página de descrição do produto
+
+  Scenario: Adicionar um produto no carrinho
+    Given Usuário está na página de descrição de seu produto
+    When Usuário adiciona seu produto no carrinho
+    Then O usuário vai para a página de confirmação de seu item no carrinho
+
+  Scenario: Prosseguir na compra do produto
+    Given Usuário está na pagina de confirmação
+    And Procede para o checkout
+    When O usuário confirma o produto
+    Then O usuário finaliza a primeira etapa da compra
+
+  Scenario: Criar uma conta
+    Given Usuario cadastra seu email
+    And Usuario confirma seu email
+    Then Usuário preenche e envia formulário de cadastro
+
+  Scenario: Finalizar a compra do produto
+    Given Usuário valida seus dados e prossegue
+    And Usuário aceita os termos de serviço e prossegue
+    And Usuário valida o total do valor de seu produto
+    When Usuário seleciona um método de pagamento e prossegue
+    Then Usuário confere a finalização da sua compra
---
+++ BDDprovaautomacao/steps/ComprarProduto.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace BDDprovaautomacao
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        [Given(@"Usuário está na Home Page")]
+        public void GivenUsuarioEstaNaHomePage()
+        {
+            
+        }
+
+        [When(@"Usuário escolhe um produto")]
+        public void WhenUsuarioEscolheUmProduto()
+        {
+            
+        }
+
+        [Then(@"É redirecionado para a página de descrição do produto")]
+        public void ThenERedirecionadoParaAPaginaDeDescricaoDoProduto()
+        {
+            
+        }
+
+        [Given(@"Usuário está na página de descrição de seu produto")]
+        public void GivenUsuarioEstaNaPaginaDeDescricaoDeSeuProduto()
+        {
+            
+        }
+
+        [When(@"Usuário adiciona seu produto no carrinho")]
+        public void WhenUsuarioAdicionaSeuProdutoNoCarrinho()
+        {
+            
+        }
+
+        [Then(@"O usuário vai para a página de confirmação de seu item no carrinho")]
+        public void ThenOUsuarioVaiParaAPaginaDeConfirmacaoDeSeuItemNoCarrinho()
+        {
+            
+        }
+
+        [Given(@"Usuário está na pagina de confirmação")]
+        public void GivenUsuarioEstaNaPaginaDeConfirmacao()
+        {
+            
+        }
+
+        [Given(@"Procede para o checkout")]
+        public void GivenProcedeParaOCheckout()
+        {
+            
+        }
+
+        [When(@"O usuário confirma o produto")]
+        public void WhenOUsuarioConfirmaOProduto()
+        {
+            
+        }
+
+        [Then(@"O usuário finaliza a primeira etapa da compra")]
+        public void ThenOUsuarioFinalizaAPrimeiraEtapaDaCompra()
+        {
+            
+        }
+
+        [Given(@"Usuario cadastra seu email")]
+        public void GivenUsuarioCadastraSeuEmail()
+        {
+            
+        }
+
+        [Given(@"Usuario confirma seu email")]
+        public void GivenUsuarioConfirmaSeuEmail()
+        {
+            
+        }
+
+        [Then(@"Usuário preenche e envia formulário de cadastro")]
+        public void ThenUsuarioPreencheEEnviaFormularioDeCadastro()
+        {
+            
+        }
+
+        [Given(@"Usuário valida seus dados e prossegue")]
+        public void GivenUsuarioValidaSeusDadosEProssegue()
+        {
+            
+        }
+
+        [Given(@"Usuário aceita os termos de serviço e prossegue")]
+        public void GivenUsuarioAceitaOsTermosDeServicoEProssegue()
+        {
+            
+        }
+
+        [Given(@"Usuário valida o total do valor de seu produto")]
+        public void GivenUsuarioValidaOTotalDoValorDeSeuProduto()
+        {
+            
+        }
+
+        [When(@"Usuário seleciona um método de pagamento e prossegue")]
+        public void WhenUsuarioSelecionaUmMetodoDePagamentoEProssegue()
+        {
+            
+        }
+
+        [Then(@"Usuário confere a finalização da sua compra")]
+        public void ThenUsuarioCo()
+        {
+            
+        }
+    }    
+}
